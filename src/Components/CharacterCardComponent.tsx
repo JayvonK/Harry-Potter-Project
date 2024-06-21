ee3b864bb5db140d0d888f9174fb31f9e9c06973
JayvonK <jaykhuth@gmail.com>
2024-06-21 15:23:15 -0700

    ancestry

--- src/Components/CharacterCardComponent.tsx
+++ src/Components/CharacterCardComponent.tsx
@@ -27,14 +27,50 @@ const CharacterCardComponent = (props: { w: IWizard }) => {
           </AlertDialogTrigger>
           <AlertDialogContent>
             <AlertDialogHeader>
-              <div className="flex">
+              <div className="flex mb-4">
                 <img className="object-cover aspect-square w-16 rounded-full" src={props.w.image.trim() !== "" ? props.w.image : props.w.name === "Albus Dumbledore" ? "/images/dumbledore.png" : "/images/blankpfp.png"} alt="" />
                 <h2 className="ml-5 text-2xl flex items-center HPFont">{props.w.name}</h2>
               </div>
-              <div className="grid grid-cols-3">
+
+              <div className="grid grid-cols-3 gap-4 gap">
+
+                  <div>
+                    <h2>House:</h2>
+                    <h3>{props.w.house === "" ? "None" : props.w.house}</h3>
+                  </div>
+
+                  <div>
+                    <h2>Birthday:</h2>
+                    <h3>{props.w.dateOfBirth  === "" ? "None" : props.w.dateOfBirth}</h3>
+                  </div>
+
+                  <div>
+                    <h2>Gender:</h2>
+                    <h3>{props.w.gender  === "" ? "None" : props.w.gender}</h3>
+                  </div>
+
+                  <div>
+                    <h2>Actor:</h2>
+                    <h3>{props.w.actor  === "" ? "None" : props.w.actor}</h3>
+                  </div>
+
+                  <div>
+                    <h2>Ancestry:</h2>
+                    <h3>{props.w.ancestry  === "" ? "None" : props.w.ancestry}</h3>
+                  </div>
+
+                  <div>
+                    <h2>Alternate Names:</h2>
+                    {
+                      props.w.alternate_names.length !== 0 ? props.w.alternate_names.map(name => (
+                        <h3>{name}</h3>
+                      )) : <h3>None</h3>
+                    }
+                  </div>
+
               </div>
             </AlertDialogHeader>
-            <AlertDialogFooter>
+            <AlertDialogFooter className="mt-4">
               <AlertDialogAction>Close</AlertDialogAction>
             </AlertDialogFooter>
           </AlertDialogContent>
