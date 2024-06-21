63bdba5cdfee6a45567744e064b35001cc61d1e5
JayvonK <jaykhuth@gmail.com>
2024-06-21 11:57:49 -0700

    different pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,8 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/Components/ui/alert-dialog"
-import { Button } from "@/Components/ui/button"
+import { Button } from "@/Components/ui/button";
+import { useToast } from "@/Components/ui/use-toast";
 
 
 export default function Home() {
@@ -24,12 +25,58 @@ export default function Home() {
 
   const [mainPage, setMainPage] = useState<boolean>(true);
   const [wizardsPage, setWizardsPage] = useState<boolean>(false);
-  const []
+  const [booksPage, setBooksPage] = useState<boolean>(false);
+  const [spellsPage, setSpellsPage] = useState<boolean>(false);
+  const [moviesPage, setMoviesPage] = useState<boolean>(false);
+
+  const { toast } = useToast();
 
   const GetAllWizards = async () => {
     const data: IWizard[] = await GetAllCharacters();
     setWizards(data);
     console.log(data);
+  }
+
+  const handleBooksClick = () => {
+    setPagesFalse();
+    setBooksPage(true);
+  }
+
+  const handleWizardsClick = () => {
+    setPagesFalse();
+    setWizardsPage(true);
+  }
+
+  const handleSpellsClick = () => {
+    setPagesFalse();
+    setSpellsPage(true);
+  }
+
+  const handleMoviesClick = () => {
+    setPagesFalse();
+    setMoviesPage(true);
+  }
+
+  const handleMainPageClick = () => {
+    setPagesFalse();
+    setMainPage(true);
+  }
+
+  const setPagesFalse = () => {
+    setBooksPage(false);
+    setWizardsPage(false);
+    setSpellsPage(false);
+    setMoviesPage(false);
+    setMainPage(false);
+  }
+
+  const errorToast = () => {
+    alert("Please Select One of the Categories in the Navigation Bar")
+    toast({
+      variant: "destructive",
+      title: "Uh oh! Something went wrong.",
+      description: "Please Select One of the Categories in the Navigation Bar",
+    })
   }
 
   useEffect(() => {
@@ -43,20 +90,20 @@ export default function Home() {
 
       <nav className="grid grid-cols-[40%_20%_40%] justify-center z-40 relative bg-black bg-opacity-20 py-4 mb-10 HPFont">
         <div className="flex justify-between items-center ml-12">
-          <h2 className="text-white text-xl hover:cursor-pointer">BOOKS</h2>
-          <h2 className="text-white text-xl hover:cursor-pointer">MOVIES</h2>
-          <h2 className="text-white text-xl hover:cursor-pointer">SPELLS</h2>
-          <h2 className="text-white text-xl hover:cursor-pointer">CHARACTERS</h2>
+          <h2 className="text-white text-xl hover:cursor-pointer" onClick={handleBooksClick}>BOOKS</h2>
+          <h2 className="text-white text-xl hover:cursor-pointer" onClick={handleMoviesClick} >MOVIES</h2>
+          <h2 className="text-white text-xl hover:cursor-pointer" onClick={handleSpellsClick}>SPELLS</h2>
+          <h2 className="text-white text-xl hover:cursor-pointer" onClick={handleWizardsClick}>WIZARDS</h2>
 
         </div>
 
         <div className="flex justify-center">
-          <img className="w-52" src="/images/Harry-Potter-Logo.png" alt="Harry Potter Logo" />
+          <img className="w-52 hover:cursor-pointer" src="/images/Harry-Potter-Logo.png" alt="Harry Potter Logo" onClick={handleMainPageClick}/>
         </div>
 
         <div className="flex items-center mr-12">
           <h2 className="text-white mr-5 text-xl">SEARCH:</h2>
-          <input className="w-full text-xl py-2 rounded-md pl-2 bg-gray-300" type="text" />
+          {mainPage ? <button className="w-full text-xl py-2 rounded-md pl-2 bg-gray-300 text-left text-gray-500" onClick={errorToast}>Type Here</button> : <input className="w-full text-xl py-2 rounded-md pl-2 bg-white" type="text" placeholder="Type Here"/>}
         </div>
 
       </nav>
@@ -66,43 +113,45 @@ export default function Home() {
         {/* <div className="HPFont text-3xl absolute text-white">Welcome to the PotterPedia</div> */}
       </div>
 
-      <div className="grid grid-cols-7 gap-4 relative z-40 mx-12 mt-20">
-        {
-          wizards.map((w, idx) => {
-            return (
-              <div key={idx} className="flex justify-center flex-col items-center rounded-md ">
-                <AlertDialog >
-                  <AlertDialogTrigger asChild>
-                    <div className="w-48 border shadow-lg shadow-gray-500 hover:shadow-yellow-500 rounded-md hover:cursor-pointer">
-                      <img className="object-cover w-full h-56" src={w.image.trim() !== "" ? w.image : "/images/Harry.jpg"} alt="" />
-                      <div className="bg-black p-6 w-full">
-                        <h1 className="text-white HPFont">{w.name}</h1>
+      {
+        wizardsPage && <div className="grid grid-cols-7 gap-4 relative z-40 mx-12 mt-20">
+          {
+            wizards.map((w, idx) => {
+              return (
+                <div key={idx} className="flex justify-center flex-col items-center rounded-md ">
+                  <AlertDialog >
+                    <AlertDialogTrigger asChild>
+                      <div className="w-48 border shadow-lg shadow-gray-500 hover:shadow-yellow-500 rounded-md hover:cursor-pointer">
+                        <img className="object-cover w-full h-56" src={w.image.trim() !== "" ? w.image : "/images/Harry.jpg"} alt="" />
+                        <div className="bg-black p-6 w-full">
+                          <h1 className="text-white HPFont">{w.name}</h1>
+                        </div>
                       </div>
-                    </div>
-                  </AlertDialogTrigger>
-                  <AlertDialogContent>
-                    <AlertDialogHeader>
-                      <div className="flex">
-                        <img className="object-cover aspect-square w-16 rounded-full" src={w.image.trim() !== "" ? w.image : "/images/Harry.jpg"} alt="" />
-                        <h2 className="ml-5 text-2xl flex items-center HPFont">{w.name}</h2>
-                      </div>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                      <AlertDialogHeader>
+                        <div className="flex">
+                          <img className="object-cover aspect-square w-16 rounded-full" src={w.image.trim() !== "" ? w.image : "/images/Harry.jpg"} alt="" />
+                          <h2 className="ml-5 text-2xl flex items-center HPFont">{w.name}</h2>
+                        </div>
 
-                      <div className="grid grid-cols-3">
+                        <div className="grid grid-cols-3">
 
-                      </div>
-                    </AlertDialogHeader>
-                    <AlertDialogFooter>
-                      <AlertDialogAction>Close</AlertDialogAction>
-                    </AlertDialogFooter>
-                  </AlertDialogContent>
-                </AlertDialog>
+                        </div>
+                      </AlertDialogHeader>
+                      <AlertDialogFooter>
+                        <AlertDialogAction>Close</AlertDialogAction>
+                      </AlertDialogFooter>
+                    </AlertDialogContent>
+                  </AlertDialog>
 
 
-              </div>
-            )
-          })
-        }
-      </div>
+                </div>
+              )
+            })
+          }
+        </div>
+      }
 
       <div className="z-50 w-full h-full bg-black opacity-80">
 
